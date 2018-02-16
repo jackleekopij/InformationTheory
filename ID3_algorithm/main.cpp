ac117caeed372880288dee5da1abb2eeb947baff
jackleekopij <jack.a.leekopij@gmail.com>
2018-02-16 16:36:02 +0800

    Keep code base up to date

--- ID3_algorithm/main.cpp
+++ ID3_algorithm/main.cpp
@@ -63,11 +63,9 @@ string calculateEntropy( vector<vector<string>> training_data, map<int,string> t
         for(auto& element:row_counts) entropy += (-1) * element.second * log2(element.second);
         entropy_store[training_data_column_map[i]] = entropy;
     }
-    for (auto& element:entropy_store) cout << "The entropy for " << element.first << " is: " << element.second << endl;
 
     double max_value = 0;
     string highest_entropy_variable = "";
-    for (auto& element:entropy_store) cout << "The entropy store is: " << element.second << endl;
     for (auto& element:entropy_store) if (element.second > max_value and element.first != "Id") {max_value = element.second; highest_entropy_variable = element.first;};
 
     return highest_entropy_variable;
@@ -75,8 +73,7 @@ string calculateEntropy( vector<vector<string>> training_data, map<int,string> t
 
 
 
-
-string getIndexOfSplitValues(vector<vector<string>> training_data, string variable_to_split, map<int,string> variable_to_col_num){
+map<string, vector<int>> getIndexOfSplitValues(vector<vector<string>> training_data, string variable_to_split, map<int,string> variable_to_col_num){
     // Get col number of the variable to split the training data on.
     int split_col_number;
     for(map<int,string>::iterator it = variable_to_col_num.begin(); it != variable_to_col_num.end(); it++){
@@ -88,17 +85,16 @@ string getIndexOfSplitValues(vector<vector<string>> training_data, string variab
     for(int i=0; i<variable_split_data.size(); i++){
         variable_data_split_by_level[variable_split_data[i]].push_back(i+1);
     }
-
-    // Print out the contents of the split data map
-    cout << "The column to split on is : " << variable_to_split << endl;
-    for(auto& element:variable_data_split_by_level) for(auto& index:element.second) cout << "The variable to split is " << variable_to_split << " with value " << element.first << " has the indexes " << index << endl;
-
-    return "Complete";
+    return variable_data_split_by_level;
 }
 
 int main() {
+    // A data structure to build the ordering of importance of variables.
+    // string: is the name of the variable that was split on
+    // vector<vector<string>>: a matrix to store the tree for it's variable's (the key) split.
+    //vector<vector<map<string,vector<int>>>> dichotimiser_tree = { };
+    vector<map<string,vector<int>>> dichotimiser_tree = { };
     // Create example training data.
-    //
     map<int,string> training_data_column_map = {{0,"Id"},{1,"stream"},{2,"slope",},{3,"elevation",},{4,"vegetation"}};
 
      vector<vector<string>> data_matrix_vectors = {{"1", "false", "steep", "high", "chapparal"},
@@ -109,26 +105,21 @@ int main() {
                                           {"6", "true", "steep", "highest", "conifer"},
                                           {"7", "true", "steep", "high", "chapparal"}};
 
-
     vector<vector<string>> transpose_test = transpose_vector_of_vectors(data_matrix_vectors);
 
     string important_variable = calculateEntropy(transpose_test, training_data_column_map, 7.0);
 
-    cout << "The important variable is: " << important_variable << endl;
 
     // Test the variable split
-    string variable_test_split = getIndexOfSplitValues(transpose_test, important_variable, training_data_column_map);
+    map<string, vector<int>> variable_test_split = getIndexOfSplitValues(transpose_test, important_variable, training_data_column_map);
 
+    cout << "Print out the current level of the tree: " << endl;
+    for (auto& element:variable_test_split) for(auto& index:element.second) cout << "The varaible have currently split on is: " << important_variable << " with the level: " << element.first << " and the index " << index << endl;
 
+    dichotimiser_tree.push_back(variable_test_split);
 
-    // print out a map value
-    cout << "The value for the integer 1 in the map: " << training_data_column_map[10] << endl;
-
-    // Print size of the training data
-
-
-    // Get remaining variables for each branch conditional on data removed.
-
+    // print the dichotmiser tree so far
+    for (auto& element:dichotimiser_tree[0]) for (auto& index:element.second)  cout << "The first levels of the dichotimiser are: " << element.first << " and the index values " << index << endl;
 
 
 
